2a59df5aeab844fbaa71a986443b448a37a0377a
99dilmimaleesha <dilmimaleesha300@gmail.com>
2025-07-15 16:17:10 +0530

    Added Mobile view

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -11,13 +11,23 @@ import SearchIcon from "@mui/icons-material/Search";
 const Navbar = () => {
   return (
     <AppBar position="static" color="default" elevation={0}>
-      <Toolbar sx={{ justifyContent: "space-between" }}>
+      <Toolbar
+        sx={{
+          flexDirection: { xs: "column", sm: "row" },
+          alignItems: { xs: "flex-start", sm: "center" },
+          justifyContent: "space-between",
+          gap: 2,
+          py: { xs: 2, sm: 1 },
+          px: { xs: 2, sm: 3 },
+        }}
+      >
         <Typography variant="h6">Adviser</Typography>
 
         <TextField
           size="small"
           placeholder="Search professionals"
           variant="outlined"
+          sx={{ width: { xs: "100%", sm: "300px" } }}
           InputProps={{
             startAdornment: (
               <InputAdornment position="start">
@@ -26,10 +36,17 @@ const Navbar = () => {
             ),
           }}
         />
-        <Button variant="outlined">Sign up</Button>
+
+        <Button
+          variant="outlined"
+          sx={{ alignSelf: { xs: "stretch", sm: "auto" }, width: { xs: "100%", sm: "auto" } }}
+        >
+          Sign up
+        </Button>
       </Toolbar>
     </AppBar>
   );
 };
 
 export default Navbar;
+
--- src/components/ProfileHeader.tsx
+++ src/components/ProfileHeader.tsx
@@ -2,43 +2,60 @@ import { Avatar, Typography, Box, Stack, Link } from "@mui/material";
 import LocationOnIcon from "@mui/icons-material/LocationOn";
 import TwitterIcon from "@mui/icons-material/Twitter";
 import LinkedInIcon from "@mui/icons-material/LinkedIn";
-import "../style/profileHeader.css";
 import { Facebook } from "@mui/icons-material";
 
 const ProfileHeader = () => {
   return (
-    <Box display="flex" alignItems="center" gap={3} className="profileheader">
+    <Box
+      display="flex"
+      flexDirection={{ xs: "column", sm: "row" }}
+      alignItems={{ xs: "center", sm: "flex-start" }}
+      gap={3}
+      px={2}
+      mt={{ xs: 2, sm: 0 }}
+    >
       <Avatar
         src="https://randomuser.me/api/portraits/men/1.jpg"
         alt="profile"
-        sx={{ width: 120, height: 120, border: "4px solid white", mt: -8 }}
+        sx={{
+          width: { xs: 100, sm: 120 },
+          height: { xs: 100, sm: 120 },
+          border: "4px solid white",
+          mt: { xs: 0, sm: -8 },
+        }}
       />
-      <Box>
+      <Box textAlign={{ xs: "center", sm: "left" }}>
         <Typography variant="h5" fontWeight="bold">
           Kevin Smith
         </Typography>
         <Typography color="text.secondary">
           Advisor and Consultant at Stripe Inc.
         </Typography>
+
         <Stack
           direction="row"
-          spacing={2}
+          spacing={1}
           mt={1}
           alignItems="center"
+          flexWrap="wrap"
+          justifyContent={{ xs: "center", sm: "flex-start" }}
           sx={{ color: "text.secondary" }}
         >
           <LocationOnIcon fontSize="small" />
           <Typography variant="body2">Nuwaraeliya, Sri Lanka</Typography>
-          <Link href="#" underline="hover">
-            <Facebook />
+
+          <Link href="#" underline="hover" color="inherit">
+            <Facebook fontSize="small" />
           </Link>
           <Typography variant="body2">Dilmi_Malee</Typography>
-          <Link href="#" underline="hover">
-            <LinkedInIcon />
+
+          <Link href="#" underline="hover" color="inherit">
+            <LinkedInIcon fontSize="small" />
           </Link>
           <Typography variant="body2">Dilmi_Malee</Typography>
-          <Link href="#" underline="hover">
-            <TwitterIcon />
+
+          <Link href="#" underline="hover" color="inherit">
+            <TwitterIcon fontSize="small" />
           </Link>
           <Typography variant="body2">Dilmi_Malee</Typography>
         </Stack>
